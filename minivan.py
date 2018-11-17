14b6e66359bb7658ea6e0b733c89c7f89af0e645
Rachel Laub <rachellaub@mail.weber.edu>
2018-11-17 23:07:02 +0000

    Mini Van test and file finished

--- minivan.py
+++ minivan.py
@@ -5,7 +5,8 @@ Module Documentation Here
 from __future__ import print_function
 
 
-def car_doors(states, gear):
+def car_doors(left_dash, right_dash, child_lock, master_lock, left_inside,
+              left_outside, right_inside, right_outside, gear):
     """
     Function recieves several input values to simulate
     the current state of a van. The values are checked
@@ -27,34 +28,59 @@ def car_doors(states, gear):
         openleft: Open Left Door (0 or 1)
         openright: Open Right Door (0 or 1)
     """
-    openleft = 0
-    openright = 0
+    states = [left_dash, right_dash, child_lock, master_lock, left_inside, left_outside,
+              right_inside, right_outside]
+
+
+    print("Left dashboard switch (0 or 1): {}".format(left_dash))
+    print("Right dashboard switch (0 or 1): {}".format(right_dash))
+    print("Child lock switch (0 or 1): {}".format(child_lock))
+    print("Master unlock switch (0 or 1): {}".format(master_lock))
+    print("Left inside handle (0 or 1): {}".format(left_inside))
+    print("Left outside handle (0 or 1): {}".format(left_outside))
+    print("Right inside handle (0 or 1): {}".format(right_inside))
+    print("Right outside handle (0 or 1): {}".format(right_outside))
+    print("Gear shift position (P, N, D, 1, 2 ,3, or R): {}".format(gear))
+    #test gear is valid
+
+    if gear not in ("P", "N", "D", "1", "2", "3", "R"):
+        print("Invalid Record: Both doors stay closed")
+        return
+
+
+    openleft = "Left door opens"
+    openright = "Right door opens"
 
     if states[3] and (gear == 'P') and states[0]:
-        openleft = 1
+        print(openleft)
+        return
     elif states[3] and (gear == 'P') and states[5]:
-        openleft = 1
+        print(openleft)
+        return
     elif states[3] and (gear == 'P') and states[4] and not states[2]:
-        openleft = 1
-    if states[3] and (gear == 'P') and states[1]:
-        openright = 1
+        print(openleft)
+        return
+    elif states[3] and (gear == 'P') and states[1]:
+        print(openright)
+        return
     elif states[3] and (gear == 'P') and states[7]:
-        openright = 1
+        print(openright)
+        return
     elif states[3] and (gear == 'P') and states[6] and not states[2]:
-        openright = 1
+        print(openright)
+        return
 
-
-    return openleft, openright
+    if gear != "P":
+        print("Both doors stay closed")
+        return
 
 def main():
     """
     Test your module
     """
-    switches = [1, 0, 1, 1, 0, 0, 1, 0]
-    shift = 'P'
-    left, right = car_doors(switches, shift)
-    print(left)
-    print(right)
+    #switches = [1, 0, 1, 1, 0, 0, 1, 0]
+    #shift = "P"
+    car_doors(1, 0, 1, 1, 0, 0, 1, 0, "P")
 
 
 if __name__ == "__main__":
--- minivantest.py
+++ minivantest.py
@@ -3,13 +3,41 @@
 Module Documentation Here
 """
 from __future__ import print_function
+from urllib.request import urlopen
+from minivan import car_doors
+
+
+def test_doors():
+    """
+    Opens up the web file
+    Grabs the car_doors function from module 1
+    and tells whether the car door is open or not
+    :Params: nothing
+    :Returns: nothing
+    """
+    url_test = urlopen('http://icarus.cs.weber.edu/~hvalle/cs3030/data/minivanTest.csv')
+    url_list = [line.decode('utf-8').strip('\n') for line in url_test]
+    #delete the top line of file which is title of columns
+    url_list.remove('H1, LD, RD, CL, ML, LI, LO, RI, RO, GS')
+
+    num = 1
+
+    for item in url_list:
+        print("Reading Record {}:".format(num))
+        #split up each of the items in the list
+        item_list = item.split(", ")
+        #delete the first value which is a row identifier
+        item_list.remove("R1")
+        car_doors(*item_list)
+        print()
+        num = num + 1
 
 
 def main():
     """
     Test your module
     """
-    pass
+    test_doors()
 
 
 if __name__ == "__main__":
